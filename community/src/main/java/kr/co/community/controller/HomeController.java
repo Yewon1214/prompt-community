b8051aadf8f1904d580321a0a38763fbfe028835
Yewon1214 <est1910@naver.com>
2022-08-11 13:25:16 +0900

    검색 조건 추가, post클래스 수정

--- community/src/main/java/kr/co/community/controller/HomeController.java
+++ community/src/main/java/kr/co/community/controller/HomeController.java
@@ -1,16 +1,49 @@
 package kr.co.community.controller;
 
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.Bucket;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    final String endPoint = "http://kr.object.gov-ncloudstorage.com";
+    final String regionName = "gov-standard";
+    final String accessKey = "7B3D51DA9E544404EE6C";
+    final String secretKey = "901BA8E298E59B16EA18F8352A1621084027616D";
+    final String bucketName = "portal-2022-test";
 
     @GetMapping("")
     public String home(Model model){
+
+        // S3 client
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+
+        try {
+            List<Bucket> buckets = s3.listBuckets();
+            System.out.println("Bucket List: ");
+            for (Bucket bucket : buckets) {
+                System.out.println("    name=" + bucket.getName() + ", creation_date=" + bucket.getCreationDate() + ", owner=" + bucket.getOwner().getId());
+            }
+        } catch (AmazonS3Exception e) {
+            e.printStackTrace();
+        } catch(SdkClientException e) {
+            e.printStackTrace();
+        }
 
 
         model.addAttribute("message", "hello world");
